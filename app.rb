67cc3ca945b045cad6667551fb25131e5c6149e4
Arthur Hwang <arthur.hwang@yale.edu>
2020-04-21 08:25:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Arthur Hwang"
+age = 23
+favorite_food = "Pizza"
+height_in_inches = 
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
